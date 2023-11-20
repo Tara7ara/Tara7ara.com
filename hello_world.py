76b9ee532b5101f2b8dcfc5de7d105365c1ee812
Taratara <98030336+Tara7ara@users.noreply.github.com>
2023-11-20 10:00:17 +0100

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+
+nombre = "Ciberaprendiz"
+edad = 25
+puntuacion = 9.5
+print("¡Hola, {nombre}! Tienes {edad} años y tu puntuación es {puntuacion}.")
+
+frutas = ["manzana", "banana", "uva"] for fruta in frutas: print(fruta)
+
+def cuadrado(numero): return numero * numero resultado = cuadrado(5)
+print("El cuadrado de 5 es {resultado}.")
